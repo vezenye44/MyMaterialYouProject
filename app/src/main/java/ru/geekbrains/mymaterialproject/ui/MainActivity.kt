6beb1d1fa2d0b8bdf5d3b79b15f62ac6201545d1
Alecsey Efimov <alicsey2000@gmail.com>
2023-01-25 15:58:09 +0300

    Добавление в MainActivity.kt механизма смены тем (Извлечение информации из sharedPreferences).

--- app/src/main/java/ru/geekbrains/mymaterialproject/ui/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/mymaterialproject/ui/MainActivity.kt
@@ -1,7 +1,7 @@
 package ru.geekbrains.mymaterialproject.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import ru.geekbrains.mymaterialproject.R
 import ru.geekbrains.mymaterialproject.databinding.MainActivityBinding
 import ru.geekbrains.mymaterialproject.ui.pictureOfTheDay.PictureOfTheDayFragment
@@ -9,15 +9,39 @@ import ru.geekbrains.mymaterialproject.ui.pictureOfTheDay.PictureOfTheDayFragmen
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = MainActivityBinding.inflate(layoutInflater)
+        getSharedPreferences(KEY_SP, MODE_PRIVATE).getString(EXTRA_THEME, THEME_STRICT).apply {
+            when (this) {
+                THEME_STRICT -> {
+                    this@MainActivity.setTheme(R.style.MyStrictTheme)
+                }
+                THEME_LIGHT -> {
+                    this@MainActivity.setTheme(R.style.MyLightTheme)
+                }
+                THEME_DARK -> {
+                    this@MainActivity.setTheme(R.style.MyDarkTheme)
+                }
+            }
+        }
+
         setContentView(binding.root)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, PictureOfTheDayFragment.newInstance())
                 .commitNow()
         }
+    }
+
+    companion object {
+        const val KEY_SP = "EXTRA_THEME_SETTINGS"
+        const val EXTRA_THEME = "EXTRA_THEME"
+        const val THEME_STRICT = "THEME_STRICT"
+        const val THEME_LIGHT = "THEME_LIGHT"
+        const val THEME_DARK = "THEME_DARK"
+
     }
 
 }
