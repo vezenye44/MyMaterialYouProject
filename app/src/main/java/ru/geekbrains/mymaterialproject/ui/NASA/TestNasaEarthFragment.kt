12da8835a0903acf367daf8f90ff02e91c01f12f
Alecsey Efimov <alicsey2000@gmail.com>
2023-02-21 18:34:10 +0300

    Работа с Анимацией во фрагменте Земля | Добавлена анимация изменения размера картинки по нажатию на нее (Прерывание анимации происходит резко | Поправить).

--- app/src/main/java/ru/geekbrains/mymaterialproject/ui/NASA/TestNasaEarthFragment.kt
+++ app/src/main/java/ru/geekbrains/mymaterialproject/ui/NASA/TestNasaEarthFragment.kt
@@ -4,25 +4,51 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
-import ru.geekbrains.mymaterialproject.R
+import androidx.transition.ChangeBounds
+import androidx.transition.ChangeImageTransform
+import androidx.transition.TransitionManager
+import androidx.transition.TransitionSet
+import ru.geekbrains.mymaterialproject.databinding.FragmentTestNasaEarthBinding
 
 
 class TestNasaEarthFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentTestNasaEarthBinding? = null
+    private val binding get() = _binding!!
+    private var isExpanded = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_test_nasa_earth, container, false)
-    }
+        _binding = FragmentTestNasaEarthBinding.inflate(inflater, container, false)
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        binding.imageView.setOnClickListener() {imageView ->
+            isExpanded = !isExpanded
+            TransitionManager.beginDelayedTransition(binding.root, TransitionSet().apply {
+                duration = 2000
+                addTransition(ChangeBounds())
+                addTransition(ChangeImageTransform())
+            })
+
+            (imageView as ImageView).scaleType =
+                if (isExpanded) ImageView.ScaleType.CENTER_CROP
+                else ImageView.ScaleType.FIT_CENTER
+
+            /*with(imageView as ImageView) {
+                *//*layoutParams.height =
+                    if (isExpanded) ViewGroup.LayoutParams.MATCH_PARENT
+                    else ViewGroup.LayoutParams.MATCH_PARENT*//*
+
+                scaleType =
+                    if (isExpanded) ImageView.ScaleType.CENTER_CROP
+                    else ImageView.ScaleType.FIT_CENTER
+            }*/
+        }
+
+        return binding.root
     }
 
     companion object {
