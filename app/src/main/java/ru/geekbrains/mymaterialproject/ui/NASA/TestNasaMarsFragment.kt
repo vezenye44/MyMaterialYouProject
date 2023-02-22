ad036cf0559265ce48bc40727e73e49be2cb27e3
Alecsey Efimov <alicsey2000@gmail.com>
2023-02-22 17:19:14 +0300

    Работа с ObjectAnimator и View.animate() во фрагменте Марс | Добавлены анимации: затемнения/остветления? изображения, вращения картинки и движения вверх-вниз блоков меню.

--- app/src/main/java/ru/geekbrains/mymaterialproject/ui/NASA/TestNasaMarsFragment.kt
+++ app/src/main/java/ru/geekbrains/mymaterialproject/ui/NASA/TestNasaMarsFragment.kt
@@ -1,29 +1,83 @@
 package ru.geekbrains.mymaterialproject.ui.NASA
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import ru.geekbrains.mymaterialproject.R
+import ru.geekbrains.mymaterialproject.databinding.FragmentTestNasaMarsBinding
 
 
 class TestNasaMarsFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentTestNasaMarsBinding? = null
+    private val binding get() = _binding!!
+    private var isExpanded = false
+    private val duration = 1000L
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_test_nasa_mars, container, false)
+        _binding = FragmentTestNasaMarsBinding.inflate(inflater, container, false)
+
+        binding.fab.setOnClickListener {
+            isExpanded = !isExpanded
+            if (isExpanded) expandFAB()
+            else collapseFAB()
+        }
+
+        return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private fun expandFAB() {
+        ObjectAnimator.ofFloat(binding.transparentBackground, View.ALPHA, 0.5f)
+            .setDuration(duration)
+            .start()
+
+        ObjectAnimator.ofFloat(binding.plusImageView, View.ROTATION, 0f, 225f)
+            .setDuration(duration)
+            .start()
+
+        ObjectAnimator.ofFloat(binding.optionOneContainer, View.TRANSLATION_Y, -130f)
+            .setDuration(duration)
+            .start()
+        ObjectAnimator.ofFloat(binding.optionOneContainer, View.ALPHA, 1f)
+            .setDuration(duration)
+            .start()
+
+        ObjectAnimator.ofFloat(binding.optionTwoContainer, View.TRANSLATION_Y, -250f)
+            .setDuration(duration)
+            .start()
+        ObjectAnimator.ofFloat(binding.optionTwoContainer, View.ALPHA, 1f)
+            .setDuration(duration)
+            .start()
+
     }
+
+    private fun collapseFAB() {
+        with(binding) {
+            plusImageView.animate()
+                .rotation(0f)
+
+            transparentBackground.animate()
+                .alpha(0f)
+                .duration = duration
+
+            optionTwoContainer.animate()
+                .alpha(0f)
+                .translationY(0f)
+                .duration = duration
+
+            optionOneContainer.animate()
+                .alpha(0f)
+                .translationY(0f)
+                .duration = duration
+
+        }
+    }
+
 
     companion object {
         @JvmStatic
