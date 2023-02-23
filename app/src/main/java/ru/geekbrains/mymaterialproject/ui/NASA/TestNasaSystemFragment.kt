193ef9395295a0716a617e383ecd406638de4a08
Alecsey Efimov <alicsey2000@gmail.com>
2023-02-23 16:56:23 +0300

    Работа с ConstraintLayout и ConstraintSet во фрагменте Система | Создание первой сцены на основе старой разметки. Создание вотрой(конечной) сцены. Обработка нажатия на экран / анимирования изменения констрэйнтов.

--- app/src/main/java/ru/geekbrains/mymaterialproject/ui/NASA/TestNasaSystemFragment.kt
+++ app/src/main/java/ru/geekbrains/mymaterialproject/ui/NASA/TestNasaSystemFragment.kt
@@ -1,28 +1,62 @@
 package ru.geekbrains.mymaterialproject.ui.NASA
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnticipateOvershootInterpolator
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.fragment.app.Fragment
+import androidx.transition.ChangeBounds
+import androidx.transition.TransitionManager
 import ru.geekbrains.mymaterialproject.R
+import ru.geekbrains.mymaterialproject.databinding.FragmentTestNasaSystemBinding
 
 
 class TestNasaSystemFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var binding: FragmentTestNasaSystemBinding
+    private var show = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_test_nasa_system, container, false)
+        binding = FragmentTestNasaSystemBinding.inflate(layoutInflater, container, false)
+
+        binding.backgroundImage.setOnClickListener { if (show) hideComponents() else
+            showComponents() }
+
+        return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private fun showComponents() {
+        show = true
+        val constraintSet = ConstraintSet()
+        constraintSet.clone(requireContext(), R.layout.fragment_test_nasa_system_second_state)
+        val transition = ChangeBounds()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.DONUT) {
+            transition.interpolator = AnticipateOvershootInterpolator(1.0f)
+        }
+        transition.duration = 1200
+        TransitionManager.beginDelayedTransition(binding.root,
+            transition)
+        constraintSet.applyTo(binding.root)
+    }
+
+    private fun hideComponents() {
+        show = false
+        val constraintSet = ConstraintSet()
+        constraintSet.clone(requireContext(), R.layout.fragment_test_nasa_system)
+        val transition = ChangeBounds()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.DONUT) {
+            transition.interpolator = AnticipateOvershootInterpolator(1.0f)
+        }
+        transition.duration = 1200
+        TransitionManager.beginDelayedTransition(binding.root,
+            transition)
+        constraintSet.applyTo(binding.root)
     }
 
     companion object {
